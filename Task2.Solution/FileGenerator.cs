85315ef2674b83dd50f0e3a789e0f37b3627d3a1
Антон Плотников <7210697@gmail.com>
2018-04-23 10:00:29 +0300

    Task2

---
+++ Task2.Solution/FileGenerator.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Task2.Solution
+{
+    public abstract class FileGenerator
+    {
+        public string WorkingDirectory { get; private set; }
+
+        public string FileExtension { get; private set; }
+
+        public FileGenerator(string WorkingDirectory, string FileExtension)
+        {
+            this.WorkingDirectory = WorkingDirectory ?? throw new ArgumentNullException(nameof(WorkingDirectory));
+            this.FileExtension = FileExtension ?? throw new ArgumentNullException(nameof(FileExtension));
+        }
+
+        public void GenerateFiles(int filesCount, int contentLength)
+        {
+            for (var i = 0; i < filesCount; ++i)
+            {
+                var generatedFileContent = this.GenerateFileContent(contentLength);
+
+                var generatedFileName = $"{Guid.NewGuid()}{this.FileExtension}";
+
+                this.WriteBytesToFile(generatedFileName, generatedFileContent);
+            }
+        }
+
+        protected abstract byte[] GenerateFileContent(int contentLength);
+
+        private void WriteBytesToFile(string fileName, byte[] content)
+        {
+            if (!Directory.Exists(WorkingDirectory))
+            {
+                Directory.CreateDirectory(WorkingDirectory);
+            }
+
+            File.WriteAllBytes($"{WorkingDirectory}//{fileName}", content);
+        }
+    }
+}
---
+++ Task2.Solution/RandomBytesFileGenerator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2.Solution
+{
+    public class RandomBytesFileGenerator : FileGenerator
+    {
+        public RandomBytesFileGenerator(string WorkingDirectory, string FileExtension) : base(WorkingDirectory, FileExtension) { }
+
+        protected override byte[] GenerateFileContent(int contentLength)
+        {
+            var random = new Random();
+
+            var fileContent = new byte[contentLength];
+
+            random.NextBytes(fileContent);
+
+            return fileContent;
+        }
+    }
+}
