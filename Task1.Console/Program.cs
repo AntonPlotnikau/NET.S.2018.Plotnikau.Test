84d0401dcdd51dc2471a0a19ab39089c8c908dce
Антон Плотников <7210697@gmail.com>
2018-04-23 09:07:44 +0300

    Task1

---
+++ Task1.Console/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1.Console
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            PasswordCheckerService checker = new PasswordCheckerService(new SqlRepository(), new DefaultValidator());
+            System.Console.WriteLine(checker.VerifyPassword(string.Empty).Item2);
+            System.Console.WriteLine(checker.VerifyPassword("123456").Item2);
+            System.Console.WriteLine(checker.VerifyPassword("123afffffffffffffff").Item2);
+            System.Console.WriteLine(checker.VerifyPassword("78003560").Item2);
+            System.Console.ReadKey();
+        }
+    }
+}
---
+++ Task1/PasswordCheckerService.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+
+namespace Task1
+{
+    public class PasswordCheckerService
+    {
+        private IRepository repository;
+        private IValidator validator;
+
+        public PasswordCheckerService(IRepository repository, IValidator validator)
+        {
+            this.repository = repository ?? throw new ArgumentNullException(nameof(repository));
+            this.validator = validator ?? throw new ArgumentNullException(nameof(validator));
+        }
+
+        public Tuple<bool, string> VerifyPassword(string password)
+        {
+            if (password == null)
+                throw new ArgumentException($"{password} is null arg");
+
+            Tuple<bool, string> validateResult = validator.IsValid(password);
+
+            if (validateResult.Item1)
+            {
+                repository.Create(password);
+            }
+
+            return validateResult;
+        }
+    }
+}
