68eef90cc6e5807bade17af4a2855ff596e264c7
Антон Плотников <7210697@gmail.com>
2018-04-23 20:10:57 +0300

    Task 6

--- Task6.Tests/CustomEnumerableTests.cs
+++ Task6.Tests/CustomEnumerableTests.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using NUnit.Framework;
+using Test6.Solution;
 
 namespace Task6.Tests
 {
@@ -16,7 +17,9 @@ namespace Task6.Tests
         {
             int[] expected = {1, 1, 2, 3, 5, 8, 13, 21, 34, 55};
 
-            Assert.Inconclusive();
+            int[] actual = Generator<int>.Generate((x, y) => x + y, 1, 1, 10).ToArray<int>();
+
+            CollectionAssert.AreEqual(expected, actual);
         }
 
         [Test]
@@ -24,15 +27,24 @@ namespace Task6.Tests
         {
             int[] expected = { 1, 2, 4, 8, 16, 32, 64, 128, 256, 512 };
 
-            Assert.Inconclusive();
+            int[] actual = Generator<int>.Generate((x, y) => 6*y-8*x, 1, 2, 10).ToArray<int>();
+
+            CollectionAssert.AreEqual(expected, actual);
         }
 
         [Test]
         public void Generator_ForSequence3()
         {
+            double eps = 0.00001;
             double[] expected = { 1, 2, 2.5, 3.3, 4.05757575757576, 4.87086926018965, 5.70389834408211, 6.55785277425587, 7.42763417076325, 8.31053343902137 };
 
-            Assert.Inconclusive();
+            double[] actual = Generator<double>.Generate((x, y) => y + x/y, 1, 2, 10).ToArray<double>();
+
+            int i = 0;
+            foreach(double value in actual)
+            {
+                Assert.IsTrue(Math.Abs(value - expected[i++]) < eps);
+            }
         }
     }
 }
--- Test6.Solution/Generator.cs
+++ Test6.Solution/Generator.cs
@@ -9,15 +9,18 @@ namespace Test6.Solution
     public static class Generator<T>
     {
         public static IEnumerable<T> Generate(Func<T, T, T> GenerateElem, T first, T second, int count)
-            => GenerateElements<T>(GenerateElem, first, second, count);
+            => GenerateElements(GenerateElem, first, second, count);
 
         private static IEnumerable<T> GenerateElements(Func<T, T, T> GenerateElem, T first, T second, int count)
         {
             yield return first;
             yield return second;
-            for (int i = 2; i < count; i++) ;
+            for (int i = 2; i < count; i++) 
             {
-
+                T item = GenerateElem(first, second);
+                yield return GenerateElem(first, second);
+                first = second;
+                second = item;
             }
         }
     }
